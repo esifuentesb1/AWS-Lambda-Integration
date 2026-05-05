68266c2e69419dc8be9799ad6d3646aab0a36fcb
thony sb <esifuentesb1@upao.edu.pe>
2026-05-05 18:56:06 -0500

    Cambios finales

--- Modules/S3/main.tf
+++ Modules/S3/main.tf
@@ -1,16 +1,10 @@
 resource "aws_s3_bucket" "this" {
   bucket = "${var.project}-${var.env}-images"
 }
-
 resource "aws_s3_bucket_notification" "notify" {
   bucket = aws_s3_bucket.this.id
-
-  depends_on = [
-    aws_sqs_queue_policy.allow_s3
-  ]
-
   queue {
-    queue_arn = aws_sqs_queue.main.arn
-    events    = ["s3:ObjectCreated:*"]
+    queue_arn     = var.sqs_queue_arn
+    events        = ["s3:ObjectCreated:*"]
   }
 }
